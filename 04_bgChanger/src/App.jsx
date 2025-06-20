7db9c236e7ae51f23f67787b782ce5d8c38a95df
hraj-amr <hraj.amr@gmail.com>
2025-06-20 18:00:54 +0000

    Day 8

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  React from 'react'
 
 function App() {
-  const [color, setColor] = useState('olive')
 
   return (
-    <div className="w-full h-screen duration-200"
-    style={{backgroundColor:"#900"}}
+    <div className='w-[200px] h-screen duration-200'
+    style={{backgroundColor:"#999"}}
     ></div>
   )
 }
